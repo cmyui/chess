b3fbd9ef80feedae3bd4dffa1651415df7e70e78
cmyui <cmyuiosu@gmail.com>
2024-06-05 07:11:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 import secrets
 from uuid import uuid4
-from app.chess import ChessGame, BoardSquare, ChessPiece, ChessPieceColor, Move
+from app.chess import (
+    ChessGame,
+    BoardSquare,
+    ChessPiece,
+    ChessPieceColor,
+    ChessPieceType,
+    Move,
+)
 
 if __name__ == "__main__":
     chess_game = ChessGame(
@@ -13,8 +20,10 @@ if __name__ == "__main__":
     move = Move(
         square_from=BoardSquare(file="E", rank="2"),
         square_to=BoardSquare(file="E", rank="4"),
-        chess_piece=ChessPiece.PAWN,
-        piece_color=ChessPieceColor.WHITE,
+        chess_piece=ChessPiece(
+            piece_type=ChessPieceType.PAWN,
+            color=ChessPieceColor.WHITE,
+        ),
     )
     chess_game.move_piece(move)
     chess_game.board.print_chess_board()
